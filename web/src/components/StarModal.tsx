2c30afc1641dd652b4f4e83d061dbdf156ba15fa
Marcel Walk <walk.marcel.97@googlemail.com>
2022-04-19 15:32:21 +0200

    chore: Reworked the support modal

--- web/src/components/StarModal.tsx
+++ web/src/components/StarModal.tsx
@@ -6,28 +6,39 @@ function StarModal() {
         position: 'fixed', bottom: 10, right: 10, zIndex: 10, background: '#5397f5', color: 'white', border: 'none',
       }}
     >
-      <header className="card-header" style={{ boxShadow: 'none' }}>
-        <p className="card-header-title" style={{ color: 'white' }}>
-          Did you know 2anki is an open-source project?
-        </p>
+      <header className="card-header columns is-centered" style={{ boxShadow: 'none', marginTop: '0px' }}>
+        <div className="column is-half">
+          <img src="./mascot/navbar-logo.png" alt="2anki mascot" />
+        </div>
+
       </header>
       <div className="card-content">
+
         <div className="content">
-          Please show us your support by starring our GitHub repo
+          <p style={{ color: 'white' }}>
+            Did you know 2anki is an open-source project?
+          </p>
+          Please consider to give us a star on GitHub!
+          <br />
+          You can support us on Patreon or GitHub sponsors too
+          <br />
+          Thanks :)
         </div>
       </div>
       <footer className="card-footer" style={{ border: 'none' }}>
         <a
           href="."
           onClick={() => {
-            alert('Dismiss clicked');
+            // eslint-disable-next-line
+            console.log('Dismiss clicked');
           }}
           className="card-footer-item"
           style={{ border: 'none', color: 'white' }}
         >
           Dismiss
         </a>
-        <a href="https://github.com/alemayhu/2anki.net" className="card-footer-item" target="_blank" rel="noreferrer" style={{ border: 'none', color: 'white' }}>Open</a>
+        <a href="https://github.com/alemayhu/2anki.net" className="card-footer-item" target="_blank" rel="noreferrer" style={{ border: 'none', color: 'white' }}>GitHub</a>
+        <a href="https://www.patreon.com/alemayhu" className="card-footer-item" target="_blank" rel="noreferrer" style={{ border: 'none', color: 'white' }}>Patreon</a>
       </footer>
     </div>
   );
